f1a7b93a4dd434b1e257d3ff1351a7e817df9717
Shrikant-Meshram <mshrikantr99@gmail.com>
2022-05-19 14:02:48 +0530

    changes

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -2,9 +2,40 @@ const express = require('express')
 const validUrl = require('valid-url')
 const shortid = require('shortid')
 const urlModel = require('../model/urlModel')
-
+const redis = require("redis");
+const { promisify } = require("util");
 
 const baseUrl = 'http:localhost:5000'
+
+
+
+//Connect to redis
+const redisClient = redis.createClient(
+    19565,
+  "redis-19565.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("ATr4txTufbPFE6LIC28LtTxvgy5U9hk8", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+
+
+
 
 const shorten = async (req, res) => {
     const { longUrl } = req.body
@@ -40,34 +71,46 @@ const shorten = async (req, res) => {
     }
 }
 
-module.exports.shorten = shorten
+
 
 /******************************************************************************** */
 
 const getUrl = async (req, res) => {
-    try {
+    try{
+         const urlCode = req.params.urlCode
 
-        const url = await urlModel.findOne({
-            urlCode: req.params.urlCode
-        })
-        if (url) {
+         const checkUrl = await GET_ASYNC(`${urlCode}`)
+         if(checkUrl){
+             return res.redirect(JSON.parse(checkUrl))
+         }
+         
+         const url = await urlModel.findOne({urlCode:urlCode})
 
-            console.log(url)
-            return res.redirect(url.longUrl)
 
-        }
-        else {
+         await SET_ASYNC(`${urlCode}`,JSON.stringify(url.longUrl))
+         return res.redirect(url.longUrl)
 
-            return res.status(404).send({status: false, message: 'No URL Found'})
-        }
-
-    }
-    // exception handler
-    catch (err) {
+    }catch(err){
         console.error(err)
-        res.status(500).send('Server Error')
+           return res.status(500).send({status:false,Error:err.message})
     }
 }
+   
+    
 
-
+module.exports.shorten = shorten
 module.exports.getUrl = getUrl
+
+
+
+
+
+
+// let cahcedProfileData = await GET_ASYNC(`${req.params.authorId}`)
+// if(cahcedProfileData) {
+//   res.send(cahcedProfileData)
+// } else {
+//   let profile = await authorModel.findById(req.params.authorId);
+//   await SET_ASYNC(`${req.params.authorId}`, JSON.stringify(profile))
+//   res.send({ data: profile });
+// }
